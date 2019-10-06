9e2303d7558df8356b166e362befd781e3b51b6f
Paul Shipley <paul.shipley@blueyonder.co.uk>
2019-10-06 23:09:58 +0100

    Adding more move logic.

--- rubiks_move.c
+++ rubiks_move.c
@@ -60,6 +60,9 @@ void rubiks_move_front_cw( rubiks_cube_t* const cube_ptr )
 {
     rubiks_move_rotate_four( &(cube_ptr->front[0][1]), &(cube_ptr->front[1][2]), &(cube_ptr->front[2][1]), &(cube_ptr->front[1][0]) );
     rubiks_move_rotate_four( &(cube_ptr->front[0][0]), &(cube_ptr->front[0][2]), &(cube_ptr->front[2][2]), &(cube_ptr->front[2][0]) );
+    rubiks_move_rotate_four( &(cube_ptr->top[2][0]), &(cube_ptr->right[0][0]), &(cube_ptr->bottom[0][2]), &(cube_ptr->left[2][2]) );
+    rubiks_move_rotate_four( &(cube_ptr->top[2][1]), &(cube_ptr->right[1][0]), &(cube_ptr->bottom[0][1]), &(cube_ptr->left[1][2]) );
+    rubiks_move_rotate_four( &(cube_ptr->top[2][2]), &(cube_ptr->right[2][0]), &(cube_ptr->bottom[0][0]), &(cube_ptr->left[0][2]) );
 }
 
 void rubiks_move_front_aw( rubiks_cube_t* const cube_ptr )
@@ -70,6 +73,9 @@ void rubiks_move_back_cw( rubiks_cube_t* const cube_ptr )
 {
     rubiks_move_rotate_four( &(cube_ptr->back[0][1]), &(cube_ptr->back[1][2]), &(cube_ptr->back[2][1]), &(cube_ptr->back[1][0]) );
     rubiks_move_rotate_four( &(cube_ptr->back[0][0]), &(cube_ptr->back[0][2]), &(cube_ptr->back[2][2]), &(cube_ptr->back[2][0]) );
+    rubiks_move_rotate_four( &(cube_ptr->top[0][2]), &(cube_ptr->left[0][0]), &(cube_ptr->bottom[2][0]), &(cube_ptr->right[2][2]) );
+    rubiks_move_rotate_four( &(cube_ptr->top[0][1]), &(cube_ptr->left[1][0]), &(cube_ptr->bottom[2][1]), &(cube_ptr->right[1][2]) );
+    rubiks_move_rotate_four( &(cube_ptr->top[0][0]), &(cube_ptr->left[2][0]), &(cube_ptr->bottom[2][2]), &(cube_ptr->right[0][2]) );
 }
 
 void rubiks_move_back_aw( rubiks_cube_t* const cube_ptr )
