74196dbac54f007a5863b1702a8acdd9b5e66eaa
Paul Shipley <paul.shipley@blueyonder.co.uk>
2020-04-12 23:53:15 +0100

    Adding a few test moves

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "rubiks.h"
+#include "rubiks_move.h"
 
 int main( void )
 {
@@ -12,16 +13,19 @@ int main( void )
     /* Initialise and display the cube */
     rubiks_initialise( my_cube );
     rubiks_show( my_cube );
+    rubiks_move_front_cw( my_cube );
+    rubiks_move_right_cw( my_cube );
+    rubiks_show( my_cube );
 
     if( rubiks_is_solved( my_cube ) )
     {
         printf("Cube is currently solved.\n");
-    }    
+    }
     else
     {
         printf("Cube is currently not solved.\n");
     }
-    
+
 
     /* Free memory */
     free( my_cube );
